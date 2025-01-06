7bf8a4f3c0db96da6d412616d1edc0f4357e1ecd
Samuel Flowers <159969397+Samuel-Flowers@users.noreply.github.com>
2025-01-06 07:26:47 -0600

    temporarily removing demo links

--- src/components/ProjectWell.tsx
+++ src/components/ProjectWell.tsx
@@ -15,7 +15,7 @@ interface ProjectWellProps {
 const ProjectWell: React.FC<ProjectWellProps> = ({ content }) => {
   return (
     <div className="lg:w-[800px] md:w-[600px] w-[350px]  flex flex-col  border rounded-lg lg:p-12 md:p-10  p-5">
-      <h1 className="text-center md:text-3xl lg:text-4xl">Github and Demo Links</h1>
+      <h1 className="text-center md:text-3xl lg:text-4xl">Github Links</h1>
       {content.map((link, index) => (
         <div
           key={index}
@@ -23,7 +23,8 @@ const ProjectWell: React.FC<ProjectWellProps> = ({ content }) => {
           <div className="flex flex-row justify-between  md:mt-10 mt-6">
             
             <a
-              href={link.demo}
+              // href={link.demo}
+              href = {link.link}
               
               target="_blank"
               className="lg:text-3xl md:text-2xl text-xl hover:text-[#CCCCCC]"
@@ -31,7 +32,7 @@ const ProjectWell: React.FC<ProjectWellProps> = ({ content }) => {
               {link.projectname}
             </a>
             <div className="flex flex-row justify-between">
-            <a
+            {/* <a
               href={link.link}
               target="_blank"
               rel="noopener noreferrer"
@@ -39,15 +40,15 @@ const ProjectWell: React.FC<ProjectWellProps> = ({ content }) => {
               className="lg:text-xl md:text-lg text-sm hover:text-[#CCCCCC] hover:underline "
             >
               github
-            </a>
-            <a
+            </a> */}
+            {/* <a
               href={link.demo}
               target="_blank"
               rel="noopener noreferrer"
               className="lg:text-xl md:text-lg text-sm px-5 hover:text-[#CCCCCC] hover:underline "
             >
               demo
-            </a>
+            </a> */}
             </div>
           </div>
           <p className="md:text-xl text-md">{link.description}</p>
